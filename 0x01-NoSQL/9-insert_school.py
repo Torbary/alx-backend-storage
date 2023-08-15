efd874c414ff6f6bbf56365ac4ce7d145f041acc
Torbary <torbarey020@gmail.com>
2023-08-15 14:11:23 +0100

    insert school using pymongo

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a MongoDB collection based on kwargs.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+            The MongoDB collection object.
+        **kwargs: Keyword arguments representing the attributes
+        of the new document.
+
+    Returns:
+        ObjectId: The _id of the newly inserted document.
+    """
+    # Insert the document into the collection
+    new_document = kwargs
+    result = mongo_collection.insert_one(new_document)
+
+    # Return the _id of the newly inserted document
+    return result.inserted_id
